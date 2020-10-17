6a6aa4f9ffbeaaa345ef62ff3409aa9bf2c22cb0
rosywijewardhana <rosywijewardhana@gmail.com>
2020-10-17 23:12:47 +0530

    Rosy's second commit

--- app/src/main/java/com/example/ticketingsystem/AccountBalanceActivity.java
+++ app/src/main/java/com/example/ticketingsystem/AccountBalanceActivity.java
@@ -2,13 +2,31 @@ package com.example.ticketingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountBalanceActivity extends AppCompatActivity {
+
+    Button recharge ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_balance);
+
+        recharge=findViewById(R.id.button);
+        recharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccountBalanceActivity.this, RechargeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
+
